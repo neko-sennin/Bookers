f1a0f24509d72f2b24c2aa4f2251a010f7047847
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-06-30 15:46:58 +0000

    layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
       flash[:notice] = "book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :new
     end
   end
