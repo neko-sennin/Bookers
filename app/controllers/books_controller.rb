59e7aa209617b720f16669565cac46fe490689f5
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-07-02 12:13:37 +0000

    RSpec

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render :new
+      render "new"
     end
   end
 
@@ -28,9 +28,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path
+    flash[:notice] = "book was successfully created."
+    else
+      render :edit
+    end
   end
   
   def destroy
